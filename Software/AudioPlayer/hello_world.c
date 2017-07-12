eceb1765d72a08926daaeade59a7651316e6ca2d
Fanzhe Meng <f25meng@uwaterloo.ca>
2017-07-12 01:20:22 -0400

    Update hello_world.c

--- Software/AudioPlayer/hello_world.c
+++ Software/AudioPlayer/hello_world.c
@@ -43,27 +43,45 @@ typedef struct{
 } SongFile;
 
 typedef enum {
-	INITIAL,
-	NOT_PLAYING,
-	NEXT_SONG,
-	PREV_SONG,
 	PLAYING,
-	PAUSED,
-	REVERSING,
-	FAST_FORWARDING
+    NOT_PLAYING
 } PlayerState;
+
+typedef enum {
+    PLAY_PAUSE,
+    STOP,
+    BACKWARD,
+    FORWARD
+} Transtion;
+
+int stateChanged = 0;
 
 /*=========================================================================*/
 /*  Globals                                                                */
 /*=========================================================================*/
 SongFile songs[MAX_SONGS];
 PlayerState pstate;
+Transition transition;
 int currentTrack;
 int songCount = 0;
 
 /*=========================================================================*/
 /*  Functions                                                              */
 /*=========================================================================*/
+
+static void putRc(FRESULT rc) {
+    const char *str =
+        "OK\0" "DISK_ERR\0" "INT_ERR\0" "NOT_READY\0" "NO_FILE\0" "NO_PATH\0"
+        "INVALID_NAME\0" "DENIED\0" "EXIST\0" "INVALID_OBJECT\0" "WRITE_PROTECTED\0"
+        "INVALID_DRIVE\0" "NOT_ENABLED\0" "NO_FILE_SYSTEM\0" "MKFS_ABORTED\0" "TIMEOUT\0"
+        "LOCKED\0" "NOT_ENOUGH_CORE\0" "TOO_MANY_OPEN_FILES\0";
+    FRESULT i;
+
+    for (i = 0; i != rc && *str; i++) {
+        while (*str++);
+    }
+    xprintf("rc=%u FR_%s\n", (uint32_t) rc, str);
+}
 
 int printTrack(int trackNumber, char *filename) {
 	// open the lcd
@@ -76,15 +94,15 @@ int printTrack(int trackNumber, char *filename) {
 	fclose(lcd);
 }
 
-// Just first sets up the menu
-void initial(){
-	currentTrack = 0;			// Ensure the current track is the first one
+void init(){
+    // Ensure the current track is the first one
+	currentTrack = 0;
 	printTrack(currentTrack, songs[currentTrack].filename);
 	pstate = NOT_PLAYING;
 }
 
 void nextSong(){
-	if(currentTrack < songCount){
+	if(currentTrack < songCount - 1){
 		currentTrack += 1;
 	} else {
 		currentTrack = 0;
@@ -105,33 +123,33 @@ void prevSong(){
 	pstate = NOT_PLAYING;
 }
 
-
-void play(){
-}
-
-static void buttonPressed(void* context, alt_u32 id) {
+static void buttonISR(void* context, alt_u32 id) {
 	//xprintf("Testing ISR\n");
-	unsigned int num = IORD(BUTTON_PIO_BASE, 0);
-	switch(num){
-
+	unsigned int n = IORD(BUTTON_PIO_BASE, 0);
+    // identify which button is pressed
+	switch (n) {
+        case 0xe:
+            transition = FORWARD;
+            // TODO: how to recognize button being held
+            break;
+        case 0xd:
+            transition = PLAY_PAUSE;
+            break;
+        case 0xb:
+            transition = STOP;
+            break;
+        case 0x7:
+            transition = BACKWARD;
+            // TODO: how to recognize button being held
+            break;
 	}
-
-	IOWR(BUTTON_PIO_BASE, 3, 0x0); // Clear stimulus_in ISR
+    // indicate the state has changed
+    stateChanged = 1;
+    // clear button ISR
+	IOWR(BUTTON_PIO_BASE, 3, 0x0);
 }
 
-static void putRc(FRESULT rc) {
-    const char *str =
-        "OK\0" "DISK_ERR\0" "INT_ERR\0" "NOT_READY\0" "NO_FILE\0" "NO_PATH\0"
-        "INVALID_NAME\0" "DENIED\0" "EXIST\0" "INVALID_OBJECT\0" "WRITE_PROTECTED\0"
-        "INVALID_DRIVE\0" "NOT_ENABLED\0" "NO_FILE_SYSTEM\0" "MKFS_ABORTED\0" "TIMEOUT\0"
-        "LOCKED\0" "NOT_ENOUGH_CORE\0" "TOO_MANY_OPEN_FILES\0";
-    FRESULT i;
 
-    for (i = 0; i != rc && *str; i++) {
-        while (*str++);
-    }
-    xprintf("rc=%u FR_%s\n", (uint32_t) rc, str);
-}
 
 // Checks a filename if it is a vaild .wav filename
 int isWav(const char *filename){
@@ -177,7 +195,7 @@ int loadSongs(char *dirName){
 	return 0;
 }
 
-int loadCurrentDir(){
+int loadSongsInCurrentDir(){
 	return loadSongs("");
 }
 
@@ -185,41 +203,56 @@ int main() {
 	// Initializations
 	FATFS disk;
 	FIL songFile;
-	int i;
 
-	// Disk init and mount
+	// "di 0" -- 'disk initialize' and mounts the drive to the ‘0’ location
 	xprintf("rc=%d\n", (uint16_t) disk_initialize((uint8_t) DRIVE));
+    // "fi 0" --  'force initialization' of drive ‘0’ location
 	putRc(f_mount((uint8_t) DRIVE, &disk));
-	putRc(loadCurrentDir());
+    // load songs in current direcotry
+	putRc(loadSongsInCurrentDir());
 
 	//Clear previous interrupts if any and register ISR
 	IOWR(BUTTON_PIO_BASE, 3, 0x0);
-	alt_irq_register(BUTTON_PIO_IRQ, (void*) 0, buttonPressed);
+	alt_irq_register(BUTTON_PIO_IRQ, (void*) 0, buttonISR);
 	IOWR(BUTTON_PIO_BASE, 2, 0xFF);
-
-	// First start off on first track
-	currentTrack = INITIAL;
-
+    
+    init();
+    // TODO: need to f_open the file
+    
 	// Main run loop
 	while(1) {
-		switch(pstate){
-		case INITIAL:
-			initial();
+        while (!stateChanged) {
+            if (pstate == NOT_PLAYING) continue;
+            // TODO: code for playing song
+        }
+        
+		switch(transition){
+		case PLAY_PAUSE:
+            if (pstate == PLAYING) pstate = NOT_PLAYING;
+            else pstate = PLAYING;
 			break;
-		case NEXT_SONG:
-
-
+		case STOP:
+            // TODO: move internal pointer in file to very beginning
+            
+            pstate = NOT_PLAYING;
 			break;
-		case PREV_SONG:
-
-
+		case BACKWARD:
+            if (pstate == NOT_PLAYING) { 
+                prevSong();
+                // TODO: need to f_open the file before playing
+            }
+            else {} // pstate == PLAYING
 			break;
-		case PLAYING:
-			break;
-		default:
+		case FORWARD:
+            if (pstate == NOT_PLAYING) { 
+                nextSong();
+                // TODO: need to f_open the file before playing
+            }
+            else {} // pstate == PLAYING
 			break;
 		}
+        
+        stateChanged = 0;
 	}
 }
-
 
